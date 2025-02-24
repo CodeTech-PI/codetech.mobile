92bdf028347c651d55c83cc8f1bd1ae1def8181f
Samarah Silva <samarah.silva@jazztech.corp>
2025-02-24 13:25:34 -0300

    Login codado sem backend

---
+++ login/app/src/main/java/com/example/code_mobile/MainActivity.kt
@@ -0,0 +1,186 @@
+package com.example.code_mobile
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.code_mobile.ui.theme.CodemobileTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            CodemobileTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Tela1(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+fun Modifier.inputPadrao() = this
+    .width(300.dp)
+    .height(40.dp)
+    .background(color = Color.White, RoundedCornerShape(10.dp))
+    .clip(RoundedCornerShape(10.dp)) // Borda arredondada
+
+@Composable
+fun Tela1(modifier: Modifier = Modifier) {
+
+    var email by remember { mutableStateOf("") }
+    var senha by remember { mutableStateOf("") }
+
+    val textPadrao = TextStyle(
+        fontSize = 20.sp,
+        color = Color.White,
+        fontStyle = FontStyle.Normal
+    )
+
+    Column(modifier = modifier
+        .fillMaxSize() // faz a mudança pegar na tela toda
+        .background(Color(0xFF1B1B1B))
+        .padding(20.dp),
+
+        horizontalAlignment = Alignment.CenterHorizontally // Centraliza horizontalmente
+
+    ) {
+        Spacer(modifier = Modifier.weight(1f)) // Centraliza verticalmente
+
+        Text(
+            text = "Lombardi",
+            style = textPadrao.copy(
+                fontSize = 40.sp, // unidade sp, somente para tamanho de texto
+                fontWeight = FontWeight.Bold
+            )
+        )
+
+        Spacer(modifier = Modifier.height(50.dp)) // espaço entre os campos
+
+        Campo (
+            titulo = "Email:",
+            valor = email,
+            onValorChange = { email = it },
+            textStyle = textPadrao
+        )
+
+        Spacer(modifier = Modifier.height(30.dp)) // espaço entre os campos
+
+        Campo (
+            titulo = "Senha:",
+            valor = senha,
+            onValorChange = { senha = it },
+            textStyle = textPadrao
+        )
+
+        Spacer(modifier = Modifier.height(30.dp)) // espaço entre os campos
+
+        Button(
+            onClick = {},
+            modifier = Modifier
+                .width(300.dp)
+                .background(color = Color.White, RoundedCornerShape(10.dp))
+                .clip(RoundedCornerShape(10.dp)) // Borda arredondada
+                .background(Color(0xFFDF0050)),
+            colors = ButtonDefaults.buttonColors(Color(0xFFDF0050)), // está mudando o front padrão do botão
+            contentPadding = PaddingValues(0.dp) // centralizando
+        ) {
+            Text(
+                text = "Entrar"
+            )
+        }
+
+        Spacer(modifier = Modifier.weight(1f)) // Empurra o próximo elemento para o final
+
+        FraseInferior(
+            frase = "Desenvolvido por ",
+            codeTech = "Codetech",
+            funText = textPadrao
+
+        )
+    }
+}
+
+@Composable // Input
+fun Campo(titulo: String, valor: String, onValorChange: (String) -> Unit, textStyle: TextStyle) {
+    Column(
+        horizontalAlignment = Alignment.Start // Alinha para esquerda
+    ) {
+        Text(text = titulo, style = textStyle) // será do tipo texto com o estilo passado no "Campo"
+
+        Spacer(modifier = Modifier.height(10.dp)) // Espaço entre título e campo
+
+        // Input
+        TextField(
+            value = valor,
+            onValueChange = onValorChange, // Atualiza o valor conforme o usuário digita
+            textStyle = textStyle.copy(color = Color.Black, fontSize = 16.sp, background = Color.Red),
+            modifier = Modifier.inputPadrao()
+        )
+    }
+}
+
+
+@Composable
+fun FraseInferior(frase: String, codeTech: String, funText: TextStyle) {
+        Text(
+            //AnnotatedString = manipular style de diferentes partes do texto com estilos diferentes
+            // metade da frase (rosa) + final da frase (verde)
+
+            text = AnnotatedString(frase) +
+                    AnnotatedString(codeTech, spanStyle = funText.copy(Color(0XFF9B00CE), fontSize = 16.sp).toSpanStyle()),
+            style = funText.copy(
+                fontSize = 16.sp
+            )
+        )
+
+}
+
+@Preview(
+    showBackground = true,
+    showSystemUi = true,
+    device = Devices.PIXEL_2
+)
+
+@Composable
+fun GreetingPreview() {
+    CodemobileTheme {
+        Tela1()
+    }
+}
