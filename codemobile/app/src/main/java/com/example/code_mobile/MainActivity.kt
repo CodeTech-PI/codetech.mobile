67445e17865c16e3241b5a6168fd931484e89bc4
gabrieloliveira0505 <gabriel.oliveira165@sptech.school>
2025-03-23 20:11:22 -0300

    dando merge com a main

--- codemobile/app/src/main/java/com/example/code_mobile/MainActivity.kt
+++ codemobile/app/src/main/java/com/example/code_mobile/MainActivity.kt
@@ -34,7 +34,7 @@ fun AppNavigation() {
                         //
     val navController = rememberNavController()
 
-    NavHost(navController = navController, startDestination = "Dashboard") {
+    NavHost(navController = navController, startDestination = "PageLogin") {
         composable("PageLogin") { TelaLogin(navController) }
         composable("Clientes") { TelaClientes(navController) }
         composable("Categorias") { TelaCategorias(navController) }
